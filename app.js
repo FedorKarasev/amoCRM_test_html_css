367c6daa7159e8273a019bb75708ea0eecbbc999
Fedor Karasev <fedorr.karasev@gmail.com>
2023-03-14 20:31:10 +0300

    finished test

---
+++ app.js
@@ -0,0 +1,7 @@
+// const header = document.querySelector('.page-header');
+// const main = document.querySelector('.main');
+// const windowHeight = document.documentElement.clientHeight;
+
+// const headerHeight = Math.floor(header.getBoundingClientRect().bottom);
+
+// main.style.height = windowHeight - headerHeight + 'px';
